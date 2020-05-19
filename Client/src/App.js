c905f38032de38c249402e93beb13fb3aeb9d5e7
rajanchawla <rajanchawla3@gmail.com>
2020-05-19 21:41:59 +0200

    Basic client server architecture added to the project

---
+++ Client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div> Hello Cloud Computing team</div>
+      </header>
+    
+    </div>
+  );
+}
+
+export default App;
